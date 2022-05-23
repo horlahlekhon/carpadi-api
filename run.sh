5a1ead0127f1293243075b77b7821102c121c4c0
Adebari Olalekan <adebari.olalekan.oluwaseun@gmail.com>
2022-05-23 17:11:00 +0100

    fix: EACCEs

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
 #supervisord --nodaemon --configuration ./supervisord.conf
-gunicorn --bind 0.0.0.0:"$ENV_PORT" -w 4 --limit-request-line 6094 --access-logfile - src.wsgi:application
+gunicorn --bind 0.0.0.0:"$PORT" -w 4 --limit-request-line 6094 --access-logfile - src.wsgi:application
