0cbf13324479ea5b605e3a23bd0529e6996b19ce
Groot <groot@grootpc>
2022-06-17 20:02:27 +0100

    changed token life time to 10hrs

--- src/config/common.py
+++ src/config/common.py
@@ -326,7 +326,7 @@ REST_FRAMEWORK = {
 
 # JWT configuration
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60 * 10),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
