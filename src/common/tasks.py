fee4b3e4bfabfeff5ab62e79501169d21f46beee
Ukpono Obott <legendsergio@gmail.com>
2022-06-20 16:45:39 +0100

    Added mailchimp transactional sdk and refactored the send email task to use mailchimp

--- src/common/tasks.py
+++ src/common/tasks.py
@@ -1,19 +1,28 @@
 from celery import task
-from django.core.mail import EmailMultiAlternatives
+# from django.core.mail import EmailMultiAlternatives
+
+import mailchimp_transactional as MailchimpTransactional
+from mailchimp_transactional.api_client import ApiClientError
 
 
 @task(name='SendEmailTask')
-def send_email_task(subject, to, default_from, email_html_message):
-    msg = EmailMultiAlternatives(
-        subject,
-        email_html_message,
-        default_from,
-        to,
-        alternatives=((email_html_message, 'text/html'),),
-    )
-    msg.send()
+def send_email_task(subject, to, default_from="admin@carpadi.com", email_html_message):
+    try:
+        mailchimp = MailchimpTransactional.Client("ncTkaSqq5KLZH0Ddux7V4w")
+        response = client.messages.send({"message": {html = email_html_message,
+                                                     subject = subject,
+                                                     from_email = default_from,
+                                                     from_name = "Hello from Carpadi",
+                                                     to = [{
+                                                        email = to
+                                                     }]}})
+        print(response)
+    except ApiClientError as error:
+        print("An exception occurred: {}".format(error.text))
 
 
 @task(name='CloseTradeTask')
 def close_trade(trade):
     ...
+
+
