f2b667096045829d88fceb3b33b5d0ec93a54368
Adebari Olalekan <adebari.olalekan.oluwaseun@gmail.com>
2022-09-21 10:12:34 +0100

    fix: ci

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
-#supervisord --nodaemon --configuration ./supervisord.conf
 gunicorn --bind 0.0.0.0:"$PORT" -w 4 --limit-request-line 6094 --access-logfile - src.wsgi:application
