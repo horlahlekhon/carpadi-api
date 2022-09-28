fd71ed82a656d1284094fa730624a7ed52a1a443
Adebari Olalekan <adebari.olalekan.oluwaseun@gmail.com>
2022-09-28 18:38:44 +0100

    feat: notification troubleshoot

--- src/common/tasks.py
+++ src/common/tasks.py
@@ -11,7 +11,6 @@ from fcm_django.models import FCMDeviceQuerySet, FCMDevice, FirebaseResponseDict
 from firebase_admin.messaging import Notification, Message
 from mailchimp_transactional.api_client import ApiClientError
 
-
 # from django.core.mail import EmailMultiAlternatives
 from src.common.utils import CustomJsonEncoder
 from src.models.models import Transaction
@@ -31,9 +30,10 @@ def send_email_notification_task(context, html_template, subject, to):
             to,
             alternatives=((email_html_message, 'text/html'),),
         )
-        return msg.send()
-    except ApiClientError as error:
-        print(f"An exception occurred: {error.text}")
+        resp = msg.send()
+        print(resp)
+    except Exception as error:
+        print(f"An exception occurred: {error}")
 
 
 @task(name='SendPushNotification')
@@ -49,16 +49,47 @@ def send_push_notification_task(context, to: str):
         )
         print(f"Notification sent success: {resp.response.success_count}, failure: {resp.response.failure_count}")
         return f"Notification sent success: {resp.response.success_count}, failure: {resp.response.failure_count}"
-    except ApiClientError as error:
-        print(f"An exception occurred: {error.text}")
+    except Exception as error:
+        print(f"An exception occurred: {error}")
+
+
+def send_push_notification_taskp(context, to: str):
+    try:
+        devices: FCMDeviceQuerySet = FCMDevice.objects.filter(user_id=to)
+        body = json.loads(json.dumps(context, cls=CustomJsonEncoder))
+        pprint(body)
+        no = Notification(title=context.get('title'), body=context.get('title'))
+        msg = Message(notification=no, data=body)
+        resp: FirebaseResponseDict = devices.send_message(
+            message=msg,
+        )
+        print(f"Notification sent success: {resp.response.success_count}, failure: {resp.response.failure_count}")
+        return f"Notification sent success: {resp.response.success_count}, failure: {resp.response.failure_count}"
+    except Exception as error:
+        print(f"An exception occurred: {error}")
+
+
+def send_email_notification_taskp(context, html_template, subject, to):
+    try:
+        if isinstance(to, str):
+            to = [to]
+        email_html_message = render_to_string(html_template, context)
+        # if settings.TESTING:
+        msg = EmailMultiAlternatives(
+            subject,
+            email_html_message,
+            settings.EMAIL_FROM,
+            to,
+            alternatives=((email_html_message, 'text/html'),),
+        )
+        resp = msg.send()
+        print(resp)
+        return resp
+    except Exception as error:
+        print(f"An exception occurred: {error}")
 
 
 @task(name='CloseTradeTask')
 def close_trade(trade):
     ...
 
-
-@task(name='Check transaction status')
-def transaction_status(transaction_id):
-    tx = Transaction.objects.get(pk=transaction_id)
-    print(tx.amount)
--- src/notifications/services.py
+++ src/notifications/services.py
@@ -77,15 +77,16 @@ def _send_email(email_notification_config, context):
     to = User.objects.get(id=context.get("user")).email
     email_html_template = email_notification_config.get('email_html_template')
     email_subject = email_notification_config.get('email_subject')
-    from src.common.tasks import send_email_notification_task
+    from src.common.tasks import send_email_notification_task, send_email_notification_taskp
 
     send_email_notification_task.delay(context, email_html_template, email_subject, to)
-
+    # send_email_notification_taskp(context, email_html_template, email_subject, to)
 
 def _send_firebase(notification_config, context):
-    from src.common.tasks import send_push_notification_task
+    from src.common.tasks import send_push_notification_task, send_push_notification_taskp
 
     send_push_notification_task.delay(context, context.get("user"))
+    # send_push_notification_taskp(context, context.get("user"))
 
 
 def notify(verb, **kwargs):
